c65a1e1d87456ae0e336474efcae6dd9d2a3cfa6
VivekRanjan2002 <lit2020047@iiitl.ac.in>
2023-12-01 11:30:19 +0530

    Added: size and dequeue method

--- Queue.java
+++ Queue.java
@@ -7,11 +7,31 @@ public class Queue {
         head=null;
         size=0;
     }
+    // add items in queue
     public void enqueue(int data){
         Node newNode=new Node(data);
         newNode.next=head;
         head=newNode;
+        size++;
     }
+    // remove element from beginning
+    public void dequeue(){
+        if(size==0) return;
+        Node prev=head;
+        Node curr=head;
+        while(curr.next!=null){
+            prev=curr;
+            curr=curr.next;
+        }
+        prev.next=null;
+        size--;
+    }
+
+    // return size of the current queue
+    public int size(){
+        return size;
+    }
+    // print the queue
     public void display (){
         Node temp=head;
         while(temp!=null){
